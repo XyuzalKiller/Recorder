250a7fea511e2730615547898623a58b196be409
XyuzalKiller <189678824+XyuzalKiller@users.noreply.github.com>
2025-12-28 14:44:09 +0700

    misc: update

--- app/src/main/kotlin/com/razertexz/rectile/MainActivity.kt
+++ app/src/main/kotlin/com/razertexz/rectile/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.razertexz.rectile
 
 import android.os.Bundle
-import android.content.Context
 import android.content.Intent
 import android.media.projection.MediaProjectionManager
 import androidx.activity.ComponentActivity
@@ -11,10 +10,6 @@ import androidx.activity.result.contract.ActivityResultContracts
 import com.razertexz.rectile.ui.HomeScreen
 
 internal class MainActivity : ComponentActivity() {
-    private val mediaProjectionManager by lazy {
-        getSystemService(Context.MEDIA_PROJECTION_SERVICE) as MediaProjectionManager
-    }
-
     private val startMediaProjection = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
         if (it.resultCode == RESULT_OK) {
             startForegroundService(Intent(this, ScreenRecordService::class.java).apply {
@@ -28,6 +23,6 @@ internal class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent { HomeScreen() }
-        startMediaProjection.launch(mediaProjectionManager.createScreenCaptureIntent())
+        startMediaProjection.launch(getSystemService(MediaProjectionManager::class.java).createScreenCaptureIntent())
     }
 }
